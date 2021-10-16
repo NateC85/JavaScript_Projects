33a284bab1608b277e948d5b6c120570d3f079ea
NateC85 <90124755+NateC85@users.noreply.github.com>
2021-10-15 19:06:37 -0600

    fix

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -3,3 +3,7 @@ function Functions() {
     var result = str.fontcolor("orange") ;
     document.getElementById("Blue_Text").innerHTML = result;
 }
+
+function displayDate() {
+    document.getElementById("time").innerHTML = Date();
+}
