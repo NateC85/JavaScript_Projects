19b8144d52018f33468aab2601562493dfbcc639
NateC85 <90124755+NateC85@users.noreply.github.com>
2021-10-15 21:13:37 -0600

    fixes
    cleared up file paths

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,11 +1,5 @@
-var x = "10" ; // defining a variable(x) & giving it a number value(10)
-var y = "40" ; // defining a variable (y) & giving it a nuber value(40)
-document.write(x * y) ; // uses an expression(x * y) and displays the result(400)
+var A = "assigning a variable a string value!"
+document.write(A) ;
 
-var a = "I will complete this Bootcamp," ; // defining a variable(a) & giving it a string value.
-var b = " & become a full stack developer!"; // difining a variable & and giving it a string value.
-var a = a.fontcolor("red") ; // turns the variables font color to red.
-var b = b.fontcolor("blue") ; //turns the variables font color to blue.
-document.write(a + b ,) ; // takes two variables & concatenates the two string values.
 
-window.alert("Hello, Instructors!") ; // creates an alert window & includes an "ok" button.
+
