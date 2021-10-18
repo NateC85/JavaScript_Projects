744a8caa1da7ddefbf379b3c4494e8ef7d5bcdf4
NateC85 <90124755+NateC85@users.noreply.github.com>
2021-10-17 23:13:48 -0600

    dict

---
+++ Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,14 @@
+document.write(typeof false) ;
+document.write("</br>") ;
+
+document.write(typeof 75) ;
+document.write("</br>")
+
+document.write(typeof "turtles") ;
+document.write("</br>") ;
+
+document.write("36" + 5) ;
+document.write("</br>") ;
+
+document.write(36 + "5") ;
+
