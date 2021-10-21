4d377f80987198ef2c9d9b244279a3fca89b8068
NateC85 <90124755+NateC85@users.noreply.github.com>
2021-10-20 21:00:37 -0600

    new

--- Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
+++ Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
@@ -11,3 +11,28 @@ function slice_Method() {
     var Section = Sentence.slice(25,44) ;
     document.getElementById("Slice").innerHTML = Section ;
 }
+
+// Defining a function. Using the getElementById() and toString() methods to return a number as a string.
+function string_Method() {
+    var x = 7839 ;
+    document.getElementById("Numbers_to_string").innerHTML = x.toString() ;
+}
+
+//Defining a function. Using the to.Precision method to format a number to a specified lenght.
+function precision_Method() {
+    var x = 2345.73465829031 ;
+    document.getElementById("Precision").innerHTML = x.toPrecision(6) ;
+}
+
+// valueOf() method returns the value of a string, representing the primitive value of a string.
+function value_Of_Method() {
+    var str = "Cowabunga!!" ;
+    document.getElementById("Value_of").innerHTML = str.valueOf() ;
+}
+
+// toFixed() method will convert a number to a string, rounding the number to a specified number of decimals.
+function to_Fixed_Method() {
+    var num = 27.56789 ;
+    var n = num.toFixed(3) ;
+    document.getElementById("To_fixed").innerHTML = n ;
+}
