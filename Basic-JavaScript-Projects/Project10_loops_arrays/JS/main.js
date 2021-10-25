8c4409aebdbf80e63c79fcc946bdc0ca98a7a176
NateC85 <90124755+NateC85@users.noreply.github.com>
2021-10-25 10:22:27 -0600

    new

--- Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
+++ Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
@@ -34,9 +34,36 @@ function cub_Players() {
     document.getElementById("Cubbies").innerHTML = "In this picture, is one of my favorite players, " + Cubs_Players[2] + "."; 
 }
 
+// Using the const keyword to create a "constant" with a local scope, to the block in which it is declared.
 function constant_function() {
     const Gaming_Console = {type: "home console", brand: "PS5", Color: "black"};
-    Gaming_Console.brand = "AlienWare PC";
-    Gaming_Console.price = "$1,200-$5,000";
+    Gaming_Console.brand = "AlienWare PC"; // Changing the property brand's value
+    Gaming_Console.price = "$1,200-$5,000"; // Adding a property to our constant.
     document.getElementById("Constant").innerHTML = "The cost of a " + Gaming_Console.brand + " can run anywhere around " + Gaming_Console.price;
 }
+
+// Utilizing the let keyword to declare variables that can have block scope.
+var n = 35;
+document.write(n);
+{
+    let n = 24;
+    document.write("<br>" + n);
+}
+document.write("<br>" + n);
+
+var n = myFunction(35, 7); // Calling function, return value will end up in n.
+
+function myFunction(a, b) {
+    return a * b; // Function returns product of a & b
+}
+
+// Creating an object, using the let keyword.
+let dog = {
+    breed: "Rottweiler",
+    color: "black and brown",
+    age: "3 years old"
+    description: function() {
+        return "This dog is " + this.age + this.color + this.breed;
+    }
+};
+document.getElementById("Dog_Object").innerHTML = dog.description();
