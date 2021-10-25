3ce7fdf52f931c70d3449a9a5e9b915981dae222
NateC85 <90124755+NateC85@users.noreply.github.com>
2021-10-24 22:20:02 -0600

    Const

--- Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
+++ Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
@@ -8,10 +8,11 @@ function Call_Loop() { // Defining a function.
     }
     document.getElementById("Loop").innerHTML = text;
 }
-
+// Using the str.lenth property to return the length of a string.
 var str = "Hellooooooooo World!";
 document.getElementById("length").innerHTML = str.length;;
 
+// Example of a for loop.
 var Fav_pies = ["Pumpkin", "Dutch Apple", "Peach", "Blueberry", "Strawberry Rhubarb"];
 var Content = "";
 var p;
@@ -23,6 +24,7 @@ function for_Loop() {
     document.getElementById("List_of_Pies").innerHTML =Content;
 }
 
+// Example of an array
 function cub_Players() {
     var Cubs_Players = [];
     Cubs_Players[0] = "Ryan Sandberg";
@@ -30,4 +32,11 @@ function cub_Players() {
     Cubs_Players[2] = "Mark Grace";
     Cubs_Players[3] = "Kerry Wood";
     document.getElementById("Cubbies").innerHTML = "In this picture, is one of my favorite players, " + Cubs_Players[2] + "."; 
+}
+
+function constant_function() {
+    const Gaming_Console = {type: "home console", brand: "PS5", Color: "black"};
+    Gaming_Console.brand = "AlienWare PC";
+    Gaming_Console.price = "$1,200-$5,000";
+    document.getElementById("Constant").innerHTML = "The cost of a " + Gaming_Console.brand + " can run anywhere around " + Gaming_Console.price;
 }
