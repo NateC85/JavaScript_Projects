0556ba76ac6f41077610f5979ba4390da8e10e1b
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-04-30 21:03:20 -0600

    Clean
    Cleaned up folder.

--- JS_practice/JS/math.js
+++
@@ -1,2 +0,0 @@
-var Simple_Math = 2 + 2;
-document.getElementById("Math").innerHTML = "2 + 2 = " + Simple_Math;
--- JavaScript Projects/JS/JavaScript_3.js
+++
@@ -1,4 +0,0 @@
-function displayType(car) {
-    var carType = car.getAttribute("data-car-type");
-    alert(carType + " was a popular car in the " + car.innerHTML + "!");
-}
