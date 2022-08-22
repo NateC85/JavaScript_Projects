6fff5db1abcd5296643b4771aad691f4ee1e00b6
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-08-21 20:38:33 -0600

    modules

---
+++ node-crash-course/global.js
@@ -0,0 +1,15 @@
+// console.log(global);
+
+// setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(intv);
+// }, 3000);
+
+// const intv = setInterval(() => {
+//     console.log('in the interval');
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+console.log(document.querySelector);
