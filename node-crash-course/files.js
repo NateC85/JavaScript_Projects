e9d50d0d6ba03c8ced05fba6383592945402e29e
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-08-21 21:34:23 -0600

    fs

--- node-crash-course/files.js
+++ node-crash-course/files.js
@@ -1,3 +1,5 @@
+// callback functions as well
+
 const fs = require('fs');
 
 // reading files
@@ -11,12 +13,25 @@ const fs = require('fs');
 // console.log('last line of code');
 
 // writing files
-fs.writeFile('./blog1.txt', "Hello World", () => {
-    console.log('file was written.')
-});
+// fs.writeFile('./blog1.txt', "Hello World", () => {
+//     console.log('file was written.')
+// });
 
 
 // directories
-
-
+if (!fs.existsSync('./assets')) {
+fs.mkdir('./assets', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('folder created!');
+});
+} else {
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder deleted');
+    })
+}
 // deleting files
