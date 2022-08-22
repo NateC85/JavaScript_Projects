ea177acc1e0ac73112e554242af47e404fb63a49
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-08-21 21:22:42 -0600

    read and write to files

--- node-crash-course/files.js
+++ node-crash-course/files.js
@@ -1,3 +1,22 @@
+const fs = require('fs');
+
 // reading files
+// fs.readFile('./blog1.txt', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+
+// console.log('last line of code');
+
+// writing files
+fs.writeFile('./blog1.txt', "Hello World", () => {
+    console.log('file was written.')
+});
 
 
+// directories
+
+
+// deleting files
