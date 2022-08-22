28f25b76a0636d1dbb87c73f4b85ce801c2f00ad
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-08-22 17:54:48 -0600

    Header content

--- node-crash-course/lesson 4/server2.js
+++ node-crash-course/lesson 4/server2.js
@@ -1,7 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    // Set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+   // Send an html file
+   fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+        console.log(err);
+        res.end();
+    }
+        // res.write(data);
+        res.end(data);
+    
+   });
+    
 });
 
 server.listen(3000, 'localhost', () => {
