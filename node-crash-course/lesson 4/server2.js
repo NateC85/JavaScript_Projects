cf6c0554c5d4df03b193478b90b7d96d3a659f09
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-08-22 18:32:56 -0600

    routing and status codes

--- node-crash-course/lesson 4/server2.js
+++ node-crash-course/lesson 4/server2.js
@@ -7,15 +7,37 @@ const server = http.createServer((req, res) => {
     // Set header content type
     res.setHeader('Content-Type', 'text/html');
 
+    // setting up a switch statement
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    };
+
    // Send an html file
-   fs.readFile('./views/index.html', (err, data) => {
+   fs.readFile(path, (err, data) => {
     if (err) {
         console.log(err);
         res.end();
-    }
+    } else {
         // res.write(data);
         res.end(data);
-    
+    }
    });
     
 });
