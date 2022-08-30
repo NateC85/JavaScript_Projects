3faf080721b822df68240b67029dc4632f66811c
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-08-29 21:06:36 -0600

    View Engines

--- node-crash-course/lesson 5-6/app.js
+++ node-crash-course/lesson 5-6/app.js
@@ -3,25 +3,27 @@ const express = require('express');
 // Express app
 const app = express();
 
+// Register view engine
+app.set('view engine', 'ejs');
+
 // Listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index', { title: 'Home' });
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>About page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
-// Redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+})
 
 // 404 Page, always put at the bottom
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname});
+    res.status(404).render('404');
 });
