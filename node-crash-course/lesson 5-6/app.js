739413a81e1aba4d82d9826943decc0c4646fcf9
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-08-30 20:27:36 -0600

    Partials

--- node-crash-course/lesson 5-6/app.js
+++ node-crash-course/lesson 5-6/app.js
@@ -10,8 +10,15 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
+const blogs = [
+  { title: 'Yoshi finds eggs ', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+];
+
+
     // res.send('<p>home page</p>');
-    res.render('index', { title: 'Home' });
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
