28abcc570b7222b1d12957aa7926da68f9e7232e
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-09-01 20:38:00 -0600

    Mongoose/Mongo cluster

--- node-crash-course/lesson 5-6/models/blog.js
+++ node-crash-course/lesson 5-6/models/blog.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Structure of documents
 const blogSchema = new Schema({
     title: {
         type: String,
@@ -15,3 +16,8 @@ const blogSchema = new Schema({
         required: true
     }
 }, { timestamps: true });
+
+// Create Model of Schema
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports = Blog;
+
