c216e790fc3148d6af7750bc5f1984dd4992e485
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-09-01 22:07:12 -0600

    Sandbox

--- node-crash-course/lesson 5-6/app.js
+++ node-crash-course/lesson 5-6/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 const { result } = require('lodash');
-
 
 // Express app
 const app = express();
 
-// Connect to Mongodb
+// Connect to Mongodb and listen for requests
 const dbURI = 'mongodb+srv://NateC85:OllieJo85!@cluster0.9tcxizm.mongodb.net/Node-tuts?retryWrites=true&w=majority'
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -20,15 +20,15 @@ app.set('view engine', 'ejs');
 
 // Middleware & Static files
 app.use(express.static('public'));
-
 app.use(morgan('dev'));
 
+// Routes
 app.get('/', (req, res) => {
-const blogs = [
-  { title: 'Yoshi finds eggs ', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-];
+    const blogs = [
+        { title: 'Yoshi finds eggs ', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    ];
 
 
     // res.send('<p>home page</p>');
@@ -40,9 +40,10 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
 
+// Blog routes
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create' });
-})
+});
 
 // 404 Page, always put at the bottom
 app.use((req, res) => {
